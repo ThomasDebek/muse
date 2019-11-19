8457e6b98e32f18b53d608fef859594513025834
tom.debek@gmail.com <tom.debek@gmail.com>
2019-11-19 19:40:32 +0100

    change my views to better

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      redirect_to root_path
+    else
+      render flash[:notice] = "Somthing is wrong try again"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :link, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+end

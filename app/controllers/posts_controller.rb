cbd7762fdb1b39f258402cdcebec43156043a92a
tom.debek@gmail.com <tom.debek@gmail.com>
2019-12-02 17:59:21 +0100

    Add user_id to my devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,14 +4,16 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.order("created_at DESC")
+    @user = User.all
   end
 
   def new
-    @post = Post.new
+    #@post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to @post
     else
@@ -44,7 +46,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :link, :description)
+    params.require(:post).permit(:title, :link, :description, :user_id)
   end
 
   def set_post
@@ -52,5 +54,7 @@ class PostsController < ApplicationController
   end
 
 end
+
+
 
 

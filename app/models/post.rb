24ad57718cbb2323968334e3cf4569eb8a73b05d
tom.debek@gmail.com <tom.debek@gmail.com>
2020-02-27 21:43:37 +0100

    seting

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
 
+  has_one_attached :image
+
   has_attached_file :image, styles: { medium: "700x500>", thumb: "250x150>", small:"160x160!>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
